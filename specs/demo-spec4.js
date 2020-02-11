d80a6700179bdd9af3dee352014a95aff85bcd29
Alister Scott <ascott@console.com.au>
2020-02-11 11:37:57 +1000

    use chrome

--- specs/demo-spec4.js
+++ specs/demo-spec4.js
@@ -11,7 +11,7 @@ describe( 'Playwright 4', function() {
 	let browser, context;
 
 	before( async function() {
-		browser = await playwright.webkit.launch();
+		browser = await playwright.chromium.launch();
 		context = await browser.newContext();
 	} );
 
